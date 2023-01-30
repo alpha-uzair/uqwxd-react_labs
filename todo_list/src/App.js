6199563a5198bfcbac4d9e0f28cabad6e24daed9
uzair <jerry.khan7886@gmail.com>
2023-01-30 14:37:41 +0500

    implemented handleSubmit function

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -5,7 +5,27 @@ const App = () => {
   const [todo, setTodo] = React.useState("");
   
   // Add the handlesubmit code here
-  
+  function handleSubmit(event){
+    event.preventDefault(); //prevents from default submitting (default react function)
+
+    //creating an object for new todo
+    const newTodo={
+      id: new Date().getTime(), //sets the id of the new todo
+      text:todo.trim(),//trims spaces before and after the text
+      completed:false
+    };
+    if(newTodo.text.length>0){
+      setTodos([...todos].concat(newTodo));//changes the state and concatenate the new todo task 
+      setTodo("");
+    }else{
+      alert(
+        "invalid task"
+      );
+      setTodo("");
+    }
+
+
+  }
   
   // Add the deleteToDo code here
 
@@ -19,10 +39,19 @@ const App = () => {
 return(
 <div className ="App">
 <h1>Todo List</h1>
-<form>
-<input type ="text" align ="right" />
+{/* onSubmit calls the handleSubmit */}
+<form onSubmit={handleSubmit}> 
+<input 
+type ="text" align ="right" 
+onChange={(evnt)=> setTodo(evnt.target.value)}
+placeholder="Add new task"
+value={todo}
+/>
 <button type ="submit">Add Todo</button>
 </form>
+{todos.map((todo)=><div className="todo" key={todo.id}>
+  <div>{todo.text}</div>
+</div>)}
 </div>
 );
 };
